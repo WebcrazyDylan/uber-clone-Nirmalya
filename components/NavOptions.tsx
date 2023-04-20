4b4cf77a3fa0777b744eec8516a0c9917c302b62
Nirmalya Nayak <n.nayak263@gmail.com>
2023-04-20 21:31:45 +0530

    disable btn if origin not present

--- components/NavOptions.tsx
+++ components/NavOptions.tsx
@@ -3,24 +3,27 @@ import React from "react";
 import tw from "twrnc";
 import { Icon } from "@rneui/base";
 import { useNavigation } from "@react-navigation/native";
+import { useSelector } from "react-redux";
+import { selectOrigin } from "../slices/navSlice";
 
 const data = [
   {
     id: "123",
     ttile: "Get a Ride",
     image: "https://links.papareact.com/3pn",
-    screen: "MapScreen",
+    screen: "MapScreen"
   },
   {
     id: "456",
     ttile: "Order Food",
     image: "https://links.papareact.com/28w",
-    screen: "EatScreen",
-  },
+    screen: "EatScreen"
+  }
 ];
 
 const NavOptions = () => {
   const navigation = useNavigation();
+  const origin = useSelector(selectOrigin);
 
   return (
     <FlatList
@@ -32,12 +35,13 @@ const NavOptions = () => {
           // @ts-ignore
           onPress={() => navigation.navigate(item.screen)}
           style={tw`p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40`}
+          disabled={!origin}
         >
-          <View>
+          <View style={tw`${!origin ? "opacity-20" : ""}`}>
             <Image
               style={{ width: 120, height: 120, resizeMode: "contain" }}
               source={{
-                uri: item.image,
+                uri: item.image
               }}
             />
             <Text style={tw`mt-2 text-lg font-semibold`}>{item.ttile}</Text>

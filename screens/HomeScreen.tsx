e850539f0c125cd59f7a1d99d32233884454c4a0
Nirmalya Nayak <n.nayak263@gmail.com>
2023-04-20 12:10:44 +0530

    added uber logo

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,15 +1,29 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Image, StyleSheet, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import React from "react";
+import tw from "twrnc";
 
 const HomeScreen = () => {
   return (
-    <SafeAreaView>
-      <Text>HomeScreen</Text>
+    <SafeAreaView style={tw`bg-white h-full`}>
+      <View style={tw`p-5`}>
+        <Image
+          source={{
+            uri: "https://links.papareact.com/gzs",
+          }}
+          style={styles.image}
+        />
+      </View>
     </SafeAreaView>
   );
 };
 
 export default HomeScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  image: {
+    width: 100,
+    height: 100,
+    resizeMode: "contain",
+  },
+});

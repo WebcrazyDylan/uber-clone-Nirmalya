e792a44c0005693f973df77d59b9d613da6b2930
Nirmalya Nayak <n.nayak263@gmail.com>
2023-04-20 20:57:44 +0530

    changed origin destn types

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -5,8 +5,12 @@ import tw from "twrnc";
 import NavOptions from "../components/NavOptions";
 import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
 import { GOOGLE_MAPS_API_KEY } from "@env";
+import { useDispatch } from "react-redux";
+import { setDestination, setOrigin } from "../slices/navSlice";
 
 const HomeScreen = () => {
+  const dispatch = useDispatch();
+
   return (
     <SafeAreaView style={tw`bg-white h-full`}>
       <View style={tw`p-5`}>
@@ -27,8 +31,14 @@ const HomeScreen = () => {
             }
           }}
           onPress={(data, details = null) => {
-            console.log(data);
-            console.log(details);
+            dispatch(
+              setOrigin({
+                location: details?.geometry.location,
+                description: data.description
+              })
+            );
+
+            dispatch(setDestination(null));
           }}
           fetchDetails={true}
           enablePoweredByContainer={false}
--- slices/navSlice.ts
+++ slices/navSlice.ts
@@ -3,31 +3,37 @@ import type { PayloadAction } from "@reduxjs/toolkit";
 import { RootState } from "../store";
 
 export interface NavState {
-  origin: number;
-  destination: number;
+  origin: {
+    location: { lat: number; lng: number };
+    description: string;
+  } | null;
+  destination: {
+    location: { lat: number; lng: number };
+    description: string;
+  } | null;
   travelTimeInformation: number;
 }
 
 const initialState: NavState = {
-  origin: 0,
-  destination: 0,
-  travelTimeInformation: 0,
+  origin: null,
+  destination: null,
+  travelTimeInformation: 0
 };
 
 export const navSlice = createSlice({
   name: "nav",
   initialState,
   reducers: {
-    setOrigin: (state, action: PayloadAction<number>) => {
+    setOrigin: (state, action) => {
       state.origin = action.payload;
     },
-    setDestination: (state, action: PayloadAction<number>) => {
+    setDestination: (state, action) => {
       state.destination = action.payload;
     },
     setTravelTimeInformation: (state, action: PayloadAction<number>) => {
       state.travelTimeInformation = action.payload;
-    },
-  },
+    }
+  }
 });
 
 export const { setOrigin, setDestination, setTravelTimeInformation } =

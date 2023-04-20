1c2f1312bebf5fad95ed31472bfa1603273e43ca
Nirmalya Nayak <n.nayak263@gmail.com>
2023-04-20 20:30:22 +0530

    .prettierrc for formatting

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -7,28 +7,34 @@ import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplet
 import { GOOGLE_MAPS_API_KEY } from "@env";
 
 const HomeScreen = () => {
-  return (
-    <SafeAreaView style={tw`bg-white h-full`}>
-      <View style={tw`p-5`}>
-        <Image
-          source={{
-            uri: "https://links.papareact.com/gzs",
-          }}
-          style={styles.image}
-        />
+    return (
+        <SafeAreaView style={tw`bg-white h-full`}>
+            <View style={tw`p-5`}>
+                <Image
+                    source={{
+                        uri: "https://links.papareact.com/gzs"
+                    }}
+                    style={styles.image}
+                />
 
-        <NavOptions />
-      </View>
-    </SafeAreaView>
-  );
+                <GooglePlacesAutocomplete
+                    nearbyPlacesAPI="GooglePlacesSearch"
+                    debounce={400}
+                    placeholder="Where From?"
+                />
+
+                <NavOptions />
+            </View>
+        </SafeAreaView>
+    );
 };
 
 export default HomeScreen;
 
 const styles = StyleSheet.create({
-  image: {
-    width: 100,
-    height: 100,
-    resizeMode: "contain",
-  },
+    image: {
+        width: 100,
+        height: 100,
+        resizeMode: "contain"
+    }
 });

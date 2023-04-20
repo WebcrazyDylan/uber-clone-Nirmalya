595dccd54d977e170c5348cd35f93f28195b514b
Nirmalya Nayak <n.nayak263@gmail.com>
2023-04-20 20:44:25 +0530

    adde google address autocomplete

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -7,34 +7,52 @@ import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplet
 import { GOOGLE_MAPS_API_KEY } from "@env";
 
 const HomeScreen = () => {
-    return (
-        <SafeAreaView style={tw`bg-white h-full`}>
-            <View style={tw`p-5`}>
-                <Image
-                    source={{
-                        uri: "https://links.papareact.com/gzs"
-                    }}
-                    style={styles.image}
-                />
+  return (
+    <SafeAreaView style={tw`bg-white h-full`}>
+      <View style={tw`p-5`}>
+        <Image
+          source={{
+            uri: "https://links.papareact.com/gzs"
+          }}
+          style={styles.image}
+        />
 
-                <GooglePlacesAutocomplete
-                    nearbyPlacesAPI="GooglePlacesSearch"
-                    debounce={400}
-                    placeholder="Where From?"
-                />
+        <GooglePlacesAutocomplete
+          styles={{
+            container: {
+              flex: 0
+            },
+            textInput: {
+              fontSize: 18
+            }
+          }}
+          onPress={(data, details = null) => {
+            console.log(data);
+            console.log(details);
+          }}
+          fetchDetails={true}
+          enablePoweredByContainer={false}
+          nearbyPlacesAPI="GooglePlacesSearch"
+          debounce={400}
+          placeholder="Where From?"
+          query={{
+            key: GOOGLE_MAPS_API_KEY,
+            language: "en"
+          }}
+        />
 
-                <NavOptions />
-            </View>
-        </SafeAreaView>
-    );
+        <NavOptions />
+      </View>
+    </SafeAreaView>
+  );
 };
 
 export default HomeScreen;
 
 const styles = StyleSheet.create({
-    image: {
-        width: 100,
-        height: 100,
-        resizeMode: "contain"
-    }
+  image: {
+    width: 100,
+    height: 100,
+    resizeMode: "contain"
+  }
 });

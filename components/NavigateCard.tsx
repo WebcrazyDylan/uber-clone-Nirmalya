1031ea91d176a1af7d2fafc62639af686211d24d
Nirmalya Nayak <n.nayak263@gmail.com>
2023-04-20 21:54:04 +0530

    navigate to RideOptionsCard after entering dest

--- components/NavigateCard.tsx
+++ components/NavigateCard.tsx
@@ -1,14 +1,67 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import tw from "twrnc";
+import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
+import { GOOGLE_MAPS_API_KEY } from "@env";
+import { useDispatch } from "react-redux";
+import { setDestination } from "../slices/navSlice";
+import { useNavigation } from "@react-navigation/native";
 
 const NavigateCard = () => {
+  const dispatch = useDispatch();
+  const navigation = useNavigation();
+
   return (
-    <View>
-      <Text>NavigateCard</Text>
-    </View>
+    <SafeAreaView style={tw`bg-white flex-1`}>
+      <Text style={tw`text-center py-5 text-xl`}>Good Morning, Nirmalya</Text>
+      <View style={tw`border-t border-gray-200 flex-shrink`}>
+        <View>
+          <GooglePlacesAutocomplete
+            styles={toInputBoxStyle}
+            onPress={(data, details = null) => {
+              dispatch(
+                setDestination({
+                  location: details?.geometry.location,
+                  description: data.description
+                })
+              );
+
+              // @ts-ignore
+              navigation.navigate("RideOptionsCard");
+            }}
+            fetchDetails={true}
+            enablePoweredByContainer={false}
+            nearbyPlacesAPI="GooglePlacesSearch"
+            debounce={400}
+            minLength={2}
+            placeholder="Where To?"
+            query={{
+              key: GOOGLE_MAPS_API_KEY,
+              language: "en"
+            }}
+          />
+        </View>
+      </View>
+    </SafeAreaView>
   );
 };
 
 export default NavigateCard;
 
-const styles = StyleSheet.create({});
+const toInputBoxStyle = StyleSheet.create({
+  container: {
+    backgroundColor: "white",
+    paddingTop: 20,
+    flex: 0
+  },
+  textInput: {
+    backgroundColor: "#DDDDDF",
+    borderRadius: 0,
+    fontSize: 18
+  },
+  textInputContainer: {
+    paddingHorizontal: 20,
+    paddingBottom: 0
+  }
+});

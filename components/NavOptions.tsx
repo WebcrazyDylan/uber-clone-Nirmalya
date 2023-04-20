63c97c36571ce960c411635be68f437dbafefabb
Nirmalya Nayak <n.nayak263@gmail.com>
2023-04-20 12:35:45 +0530

    added package  add react-native-vector-icons

--- components/NavOptions.tsx
+++ components/NavOptions.tsx
@@ -1,5 +1,6 @@
-import { FlatList, Text, TouchableOpacity, View } from "react-native";
+import { FlatList, Text, TouchableOpacity, View, Image } from "react-native";
 import React from "react";
+import tw from "twrnc";
 
 const data = [
   {
@@ -20,10 +21,19 @@ const NavOptions = () => {
   return (
     <FlatList
       data={data}
+      keyExtractor={(item) => item.id}
       horizontal
       renderItem={({ item }) => (
-        <TouchableOpacity>
-          <Text>{item.ttile}</Text>
+        <TouchableOpacity style={tw`p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40`}>
+          <View>
+            <Image
+              style={{ width: 120, height: 120, resizeMode: "contain" }}
+              source={{
+                uri: item.image,
+              }}
+            />
+            <Text style={tw`mt-2 text-lg font-semibold`}>{item.ttile}</Text>
+          </View>
         </TouchableOpacity>
       )}
     />

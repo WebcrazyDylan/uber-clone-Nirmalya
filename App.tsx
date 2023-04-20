442d3ed226820ed090c13158e5d258973f3ffa1a
Nirmalya Nayak <n.nayak263@gmail.com>
2023-04-20 11:58:33 +0530

    added react-native-safe-area-context

--- App.tsx
+++ App.tsx
@@ -3,23 +3,12 @@ import { StyleSheet, Text, View } from "react-native";
 
 import { store } from "./store";
 import { Provider } from "react-redux";
+import HomeScreen from "./screens/HomeScreen";
 
 export default function App() {
   return (
     <Provider store={store}>
-      <View style={styles.container}>
-        <Text>Let's Build Uber!</Text>
-        <StatusBar style="auto" />
-      </View>
+      <HomeScreen />
     </Provider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
---
+++ screens/HomeScreen.tsx
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import React from "react";
+
+const HomeScreen = () => {
+  return (
+    <SafeAreaView>
+      <Text>HomeScreen</Text>
+    </SafeAreaView>
+  );
+};
+
+export default HomeScreen;
+
+const styles = StyleSheet.create({});

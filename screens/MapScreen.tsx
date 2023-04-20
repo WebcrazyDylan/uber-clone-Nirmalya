610b6284a6b7c55d6366593a17ab23c9567d0700
Nirmalya Nayak <n.nayak263@gmail.com>
2023-04-20 21:35:29 +0530

    added NavigateCard

--- screens/MapScreen.tsx
+++ screens/MapScreen.tsx
@@ -2,15 +2,37 @@ import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 import tw from "twrnc";
 import Map from "../components/Map";
+import { createStackNavigator } from "@react-navigation/stack";
+import NavigateCard from "../components/NavigateCard";
 
 const MapScreen = () => {
+  const Stack = createStackNavigator();
+
   return (
     <View>
       <View style={tw`h-1/2`}>
         <Map />
       </View>
 
-      <View style={tw`h-1/2`}></View>
+      <View style={tw`h-1/2`}>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="NavigateCard"
+            component={NavigateCard}
+            options={{
+              headerShown: false
+            }}
+          />
+
+          <Stack.Screen
+            name="NavigateCard"
+            component={NavigateCard}
+            options={{
+              headerShown: false
+            }}
+          />
+        </Stack.Navigator>
+      </View>
     </View>
   );
 };

e3d2a3de374baf446489497ed141695969ebaeaf
Nirmalya Nayak <n.nayak263@gmail.com>
2023-04-20 22:09:32 +0530

    fit route to screen view using useRef

--- components/Map.tsx
+++ components/Map.tsx
@@ -1,5 +1,5 @@
 import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import tw from "twrnc";
 import MapView, { Marker } from "react-native-maps";
 import { useSelector } from "react-redux";
@@ -7,12 +7,22 @@ import { selectDestination, selectOrigin } from "../slices/navSlice";
 import MapViewDirections from "react-native-maps-directions";
 import { GOOGLE_MAPS_API_KEY } from "@env";
 
-const Map = () => {
+const Map: React.FC = () => {
   const origin = useSelector(selectOrigin);
   const destination = useSelector(selectDestination);
+  const mapRef = useRef<MapView | null>(null);
+
+  useEffect(() => {
+    if (!origin || !destination) return;
+
+    mapRef!.current!.fitToSuppliedMarkers(["origin", "destination"], {
+      edgePadding: { top: 50, right: 50, bottom: 50, left: 50 }
+    });
+  }, [origin, destination]);
 
   return (
     <MapView
+      ref={mapRef}
       style={tw`flex-1`}
       mapType="mutedStandard"
       initialRegion={{
@@ -41,6 +51,18 @@ const Map = () => {
           title="Origin"
           description={origin.description}
           identifier="origin"
+        />
+      )}
+
+      {destination?.location && (
+        <Marker
+          coordinate={{
+            latitude: destination.location.lat,
+            longitude: destination.location.lng
+          }}
+          title="Origin"
+          description={destination.description}
+          identifier="destination"
         />
       )}
     </MapView>

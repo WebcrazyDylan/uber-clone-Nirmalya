2eda644c28e316348da6c3d67571d1e75720add0
Nirmalya Nayak <n.nayak263@gmail.com>
2023-04-21 07:55:41 +0530

    added rides and eats tab on btm

--- components/NavigateCard.tsx
+++ components/NavigateCard.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import tw from "twrnc";
@@ -8,6 +8,7 @@ import { useDispatch } from "react-redux";
 import { setDestination } from "../slices/navSlice";
 import { useNavigation } from "@react-navigation/native";
 import NavFavourites from "./NavFavourites";
+import { Icon } from "@rneui/base";
 
 const NavigateCard = () => {
   const dispatch = useDispatch();
@@ -45,9 +46,33 @@ const NavigateCard = () => {
             }}
           />
         </View>
+        <NavFavourites />
       </View>
 
-      <NavFavourites />
+      <View
+        style={tw`flex-row bg-white justify-evenly py-3 mt-auto border-t border-gray-100`}
+      >
+        <TouchableOpacity
+          // @ts-ignore
+          onPress={() => navigation.navigate("RideOptionsCard")}
+          style={tw`flex flex-row justify-between w-24 px-4 py-3 rounded-full bg-black`}
+        >
+          <Icon name="car" type="font-awesome" color="white" size={16} />
+          <Text style={tw`text-white text-center`}>Rides</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          style={tw`flex justify-between flex-row w-24 px-4 py-3 rounded-full`}
+        >
+          <Icon
+            name="fast-food-outline"
+            type="ionicon"
+            color="black"
+            size={16}
+          />
+          <Text style={tw`text-center`}>Eats</Text>
+        </TouchableOpacity>
+      </View>
     </SafeAreaView>
   );
 };

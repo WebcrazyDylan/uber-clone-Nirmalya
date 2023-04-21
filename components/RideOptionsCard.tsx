cbbd35b025c21d1456968cd30406d509f99fbe9d
Nirmalya Nayak <n.nayak263@gmail.com>
2023-04-21 08:21:05 +0530

    added styling to ride options

--- components/RideOptionsCard.tsx
+++ components/RideOptionsCard.tsx
@@ -1,9 +1,37 @@
 import React from "react";
 import tw from "twrnc";
-import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import {
+  FlatList,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+  Image
+} from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { Icon } from "@rneui/base";
 import { useNavigation } from "@react-navigation/native";
+
+const data = [
+  {
+    id: "Uber-X",
+    title: "Uber X",
+    multiple: 1,
+    image: "https://links.papareact.com/3pn"
+  },
+  {
+    id: "Uber-XL",
+    title: "Uber XL",
+    multiple: 1.2,
+    image: "https://links.papareact.com/5w8"
+  },
+  {
+    id: "Uber-LUX",
+    title: "Uber LUX",
+    multiple: 1.75,
+    image: "https://links.papareact.com/7pf"
+  }
+];
 
 const RideOptionsCard = () => {
   const navigation = useNavigation();
@@ -20,6 +48,33 @@ const RideOptionsCard = () => {
         </TouchableOpacity>
         <Text style={tw`text-center py-5 text-xl`}>Select a Ride</Text>
       </View>
+
+      <FlatList
+        data={data}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item: { id, title, multiple, image }, item }) => (
+          <TouchableOpacity
+            style={tw`flex-row items-center justify-between px-10`}
+          >
+            <Image
+              source={{
+                uri: image
+              }}
+              style={{
+                width: 100,
+                height: 100,
+                resizeMode: "contain"
+              }}
+            />
+
+            <View style={tw`-ml-6`}>
+              <Text style={tw`text-xl font-semibold`}>{title}</Text>
+              <Text style={tw``}>Travel Time...</Text>
+            </View>
+            <Text style={tw`text-xl`}>Money</Text>
+          </TouchableOpacity>
+        )}
+      />
     </SafeAreaView>
   );
 };

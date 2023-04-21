ba17e4d9ef01c3cba4517728e1a7391054065365
Nirmalya Nayak <n.nayak263@gmail.com>
2023-04-21 08:09:24 +0530

    added title and go back btn

--- components/RideOptionsCard.tsx
+++ components/RideOptionsCard.tsx
@@ -1,11 +1,26 @@
-import { StyleSheet, Text, View } from "react-native";
 import React from "react";
+import tw from "twrnc";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { Icon } from "@rneui/base";
+import { useNavigation } from "@react-navigation/native";
 
 const RideOptionsCard = () => {
+  const navigation = useNavigation();
+
   return (
-    <View>
-      <Text>RideOptionsCard</Text>
-    </View>
+    <SafeAreaView style={tw`-pt-8 bg-white flex-grow`}>
+      <View style={tw``}>
+        <TouchableOpacity
+          // @ts-ignore
+          onPress={() => navigation.navigate("NavigateCard")}
+          style={tw`absolute top-3 left-5 p-2 rounded-full z-50`}
+        >
+          <Icon name="chevron-left" type="fontawesome" />
+        </TouchableOpacity>
+        <Text style={tw`text-center py-5 text-xl`}>Select a Ride</Text>
+      </View>
+    </SafeAreaView>
   );
 };
 
